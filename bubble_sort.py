7242b0579c8d7c03e8b85545b4ef72aeeb4ca579
Alexander Kanunnikov <nexonium@gmail.com>
2021-11-23 02:37:10 +0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,17 +1,26 @@
-
 # Bubble sort v.0.1
 # Made by the greatest programmer of all time
 
-list = list()
-list_length = int(input("Please enter array length: \n"))
-for item in range(list_length):
-  i = int(input(f'Please enter number in array № {item+1}: \n'))
-  list.append(i)
-print(list)
+array = list()
+array_length = int(input("Please enter array length: \n"))
+for index in range(array_length):
+    i = int(input(f'Please enter number in array index {index}: \n'))
+    array.append(i)
+print(array)
 
 
-def bubble_sorter():
-  for item in list:
-    i = list[item]
-    print(i)
-bubble_sorter()
+def bubble_sort():
+    for index in range(len(array)):
+        print(index, array[index])
+        if index > 0:
+            if array[index - 1] < array[index]:
+                i = array[index]
+                i2 = array[index - 1]
+                array[index] = i2
+                array[index - 1] = i
+                print('Sorted:', index, 'Value:', array[index], 'To:', array[index - 1])
+    print(array)
+    for index in range(len(array)):
+        print('Result: ', index, array[index])
+
+bubble_sort()
